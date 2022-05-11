5a09f2e937a758ed1cec455c79a57662dd5b0864
JunHyeok <98952450+Hyeok-06@users.noreply.github.com>
2022-05-11 18:32:57 +0900

    수정해서 다시올림

--- Assets/02_Scripts/FollowPlayer.cs
+++ Assets/02_Scripts/FollowPlayer.cs
@@ -6,11 +6,9 @@ public class FollowPlayer : MonoBehaviour
 {
     public Vector3 offset = Vector3.zero;
     public GameObject targetPosition;
-    public string _targetName = "Cube";
-
     void Update()
     {
-        transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition.transform.position, 0.01f);
+        transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition.transform.position, 0.02f);
     }
 }
 
