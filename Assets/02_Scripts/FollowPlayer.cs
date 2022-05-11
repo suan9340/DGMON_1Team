735a3865a3c8554c851e450835fb344aabd4e887
JunHyeok <98952450+Hyeok-06@users.noreply.github.com>
2022-05-11 18:30:49 +0900

    플레이어 따라가는 몹 부드러운 모션

--- Assets/02_Scripts/FollowPlayer.cs
+++ Assets/02_Scripts/FollowPlayer.cs
@@ -5,34 +5,12 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Vector3 offset = Vector3.zero;
-    public Transform playerTrn = null;
     public GameObject targetPosition;
-    Vector3 vel = Vector3.zero;
-    //update에 사용할 때
-    private void Update()
+    public string _targetName = "Cube";
+
+    void Update()
     {
-        /*transform.position = Vector3.Lerp(transform.position, playerTrn.position, 3f);*/
-        transform.position = playerTrn.position + offset;
-        transform.position = Vector3.SmoothDamp(gameObject.transform.position, targetPosition.transform.position, ref vel, 1f);
-    }
-
-
-    //Coroutine + Lerp 사용할때
-    private void B()
-    {
-        StartCoroutine(A());
-    }
-
-    private IEnumerator A()
-    {
-        float time = 0;
-        while (time < 1)
-        {
-            time += Time.deltaTime * 3f;
-            transform.position = Vector3.Lerp(transform.position, playerTrn.position, time);
-
-        }
-        yield return null;
-        yield return new WaitForSeconds(0.4f);
+        transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition.transform.position, 0.01f);
     }
 }
+
